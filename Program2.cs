fd9f0a032bf9703cdee8f1d2ece3e079b184854e
fouragreements <ajithashwarya@gmail.com>
2025-01-16 09:46:00 +0530

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace assignment_qs2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Taking user input for the number and the width
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the width: ");
+            int width = int.Parse(Console.ReadLine());
+
+            // Displaying the triangle using the input number and width
+            for (int i = 1; i <= width; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(number + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
